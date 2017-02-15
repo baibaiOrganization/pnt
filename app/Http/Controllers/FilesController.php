da0619c8737674d83ce733ea08bbde659909e140
Juan2Ramos <juan2ramos@gmail.com>
2017-02-14 20:12:30 -0500

    controller

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -11,12 +11,16 @@ class FilesController extends Controller
     {
 
         if ($request->ajax()) {
-            return $request->file();
 
+            $validator = Validator::make($request->all(), [
+                'person.*.email' => 'email|unique:users',
+                'person.*.first_name' => 'required_with:person.*.last_name',
+            ]);
+            return $validator;
             foreach ($request->file() as $file) {
                 $fileName = str_random(15) . '-' . $file->getClientOriginalName();
                 $file->move(base_path() . '/public/temp/', $fileName);
-                return ['route' => '/temp/' . $fileName];
+                return ['route' => '/temp/' . $fileName, 'success' => 1];
             }
         }
     }
--- public/js/tempFiles.js
+++ public/js/tempFiles.js
@@ -13,47 +13,33 @@ $('input[type="file"]').on('change', function () {
     var typeFile = data[0].type;
 
 
-    if (typeFile == 'application/x-zip-compressed') {
-        typeFile = 'application/zip';
-        console.log(typeFile)
-        console.log('si')
-    } else {
-        console.log(typeFile)
-        console.log('no')
-    }
+    file.append('file', data[0]);
+    file.append('_token', $('#token').val());
 
+    $.ajax({
+        url: $('#url').val(),
+        type: 'POST',
+        contentType: false,
+        data: file,
+        processData: false,
+        cache: false,
+        beforeSend: function () {
+            $('.preload').removeClass("hidden");
+        },
+        success: function (data) {
 
-    if (regex.test(typeFile)) {
-
-        file.append('file', data[0]);
-        file.append('_token', $('#token').val());
-
-        $.ajax({
-            url: $('#url').val(),
-            type: 'POST',
-            contentType: false,
-            data: file,
-            processData: false,
-            cache: false,
-            beforeSend: function () {
-                $('.preload').removeClass("hidden");
-            },
-            success: function (data) {
-                console.log('Se sube archivo de manera exitosa.');
-                element.previousElementSibling.innerText = data.route;
-                element.nextElementSibling.value = data.route;
-                setTimeout(function () {
-                    $('.preload').addClass("hidden");
-                }, 1000);
-            },
-            error: function (error) {
-                console.log(error);
+            console.log(data);
+            console.log('Se sube archivo de manera exitosa.');
+            element.previousElementSibling.innerText = data.route;
+            element.nextElementSibling.value = data.route;
+            setTimeout(function () {
                 $('.preload').addClass("hidden");
-                alert('Error al cargar los archivos. Por favor vuelva a intentarlo.');
-            }
-        });
-    }
-    else {
-        alert('Tipo de archivo no permitido.');
-    }
+            }, 1000);
+        },
+        error: function (error) {
+            console.log(error);
+            $('.preload').addClass("hidden");
+            alert('Error al cargar los archivos. Por favor vuelva a intentarlo.');
+        }
+    });
 });
