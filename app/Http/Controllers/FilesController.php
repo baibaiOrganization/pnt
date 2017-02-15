3277581ba03c198db065a5a77bb8a0400625ffce
Juan2Ramos <juan2ramos@gmail.com>
2017-02-14 20:21:50 -0500

    controller

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -12,6 +12,11 @@ class FilesController extends Controller
 
         if ($request->ajax()) {
 
+
+
+            if ($this->validation($request)->fails())
+                return ['success' => 'false'];
+
             $validator = Validator::make($request->all(), [
                 'person.*.email' => 'email|unique:users',
                 'person.*.first_name' => 'required_with:person.*.last_name',
@@ -24,4 +29,21 @@ class FilesController extends Controller
             }
         }
     }
+
+    private function validation($request)
+    {
+        return Validator::make($request->all(), [
+            'cantidad' => 'required',
+            "valor" => "required",
+            "id_ciudad_origen" => "required",
+            "id_ciudad_destino" => "required",
+            "peso_fisico" => "required",
+            "packing.largo" => "required",
+            "packing.ancho" => "required",
+            "packing.alto" => "required",
+            "packing.cantidad" => "required",
+            "cantidad" => "required"
+        ]);
+    }
+
 }
